00e634208c07d48bf8d3a52891221367bf1651f8
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-05 12:42:12 +0300

    add cod

--- style.js
+++ style.js
@@ -1,11 +1,11 @@
-const calculate = (a, b, result) => {
-    if (result === '+') {
+const calculate = (a, b, operation) => {
+    if (operation === '+') {
         return a + b;
-    } else if (result === '/') {
+    } else if (operation === '/') {
         return a / b;
-    } else if (result === '-') {
+    } else if (operation === '-') {
         return a - b;
-    } else if (result === '*') {
+    } else if (operation === '*') {
         return a * b;
     } else {
         return 'error';
@@ -14,3 +14,22 @@ const calculate = (a, b, result) => {
     
 (calculate(5, 3, '+'));
 (calculate(3, 3, '/'));
+
+
+const calculate2 = (a, b, operation) => {
+    switch (operation) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return 'error';
+    }
+}
+
+(calculate2(5, 3, '+'));
+(calculate2(3, 3, '/'));
